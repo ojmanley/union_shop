1307892de8ce7fec46fca6f60876bbdb739c8cba
Owen J Manley <owenjmanley@gmail.com>
2025-12-04 23:38:25 +0000

    allowed about_page to be accessed from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/product_page.dart';
+import 'package:union_shop/about_page.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -21,7 +22,7 @@ class UnionShopApp extends StatelessWidget {
       initialRoute: '/',
       // When navigating to '/product', build and return the ProductPage
       // In your browser, try this link: http://localhost:49856/#/product
-      routes: {'/product': (context) => const ProductPage()},
+      routes: {'/product': (context) => const ProductPage(), '/about': (context) => const AboutPage()},
     );
   }
 }
